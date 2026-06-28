f71930cc9d021a0efd14e4c5fee2e6963c0c27d5
aananyaph <ananyap3434@gmail.com>
2026-06-28 19:53:21 +0530

    Initial project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    api
+      .get("/")
+      .then((res) => {
+        setMessage(res.data.message);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Finance Manager</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
